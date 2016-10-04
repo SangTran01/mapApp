c8e021d1091aeb7966e411a071b6f4c984474454
sang tran <tony_633@hotmail.com>
2016-10-04 16:29:24 -0400

    added yelpInfo

--- js/script2.js
+++ js/script2.js
@@ -3,8 +3,7 @@ var locations = [
         {
           latLng: {lat: 43.648516, lng: -79.396062},
           name:'Alo Restaurant',
-          desc:'Bars, French',
-          id: 0
+          desc:'Bars, French'
         },
         {
           latLng: {lat: 43.659299, lng: -79.382144},
@@ -33,7 +32,6 @@ var Location = function Location(location, map) {
     this.latLng = ko.observable(location.latLng);
     this.name = ko.observable(location.name);
     this.desc = ko.observable(location.desc);
-    this.id = ko.observable(location.id);
 
     var marker;
 
@@ -59,8 +57,7 @@ var Location = function Location(location, map) {
     marker.addListener('click', function() {
       var self = this;
       populateInfoWindow(self, largeInfowindow);
-      self.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){ self.setAnimation(null); }, 1450);
+      markerBounce(self);
     });
 
     // Two event listeners - one for mouseover, one for mouseout,
@@ -103,10 +100,12 @@ var viewModel = function(map){
     })
 
 
+    // Calling getYelpInfo HERE
     for(var i =0; i < self.locationList().length; i++){
-      var results = getYelpInfo(self.locationList()[i]);
-
+      getYelpInfo(self.locationList()[i]);
     }
+
+
 
 
 
@@ -123,21 +122,54 @@ var viewModel = function(map){
         });
     });
 
-    self.openInfowindow = function(location) {
-        google.maps.event.trigger(location.marker, 'click');
+    self.bounceUp = function(place) {
+      google.maps.event.trigger(place.marker, 'click');
+      markerBounce(place.marker);
     }
 
 
 }
+// end of ViewModel
+
+
+function markerBounce(marker) {
+  marker.setAnimation(google.maps.Animation.BOUNCE);
+  setTimeout(function(){marker.setAnimation(null); }, 1450);
+}
+
 // This function populates the infowindow when the marker is clicked. We'll only allow
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
 function populateInfoWindow(marker, infowindow) {
 // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
+      var yelpInfo = marker.yelpInfo.businesses[0];
+      console.log(yelpInfo);
+
+      var contentString =
+      '<div id="content">' + 
+        '<h1 id="firstHeading" class="firstHeading">' + marker.name + '</h1>' +
+        '<div id="bodyContent">' +
+        '<p> Genre: <b>'+ marker.desc + '</b></p>' +
+        '<p><b>From Yelp</b></p>' +
+        '<p>' + yelpInfo.location.display_address[0] + '</p>' +
+        '<p>' + yelpInfo.location.display_address[1] + '</p>' +
+        '<p>' + yelpInfo.location.display_address[2] + '</p>' +
+        '<p>' + yelpInfo.location.display_address[3] + '</p>' +
+        '<p>' + yelpInfo.location.display_address[4] + '</p>' +
+        '<p><b>Rating</b></p>' +
+        '<img id="yelpRating" src="' + yelpInfo.rating_img_url + '"/>' +
+        '<img id="yelpImg" src="' + yelpInfo.image_url + '"/>' +
+        '<p><b>Phone Number</b></p>' +
+        '<p>'+ yelpInfo.display_phone + '</p>' +
+        '</div>' +
+      '</div>';
+
       infowindow.marker = marker;
-      infowindow.setContent('<div>' + '<b>' +marker.name + '</b>' + "<br>"+ marker.desc + '</div>');
+      infowindow.setContent(contentString);
+
       infowindow.open(map, marker);
+
       // Make sure the marker property is cleared if the infowindow is closed.
       infowindow.addListener('closeclick', function() {
         infowindow.marker = null;
@@ -162,19 +194,16 @@ function makeMarkerIcon(markerColor) {
 }
 
 
-
-
-
 function initMap() {
     var googleMap = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 43.6532, lng: -79.3832},
       zoom: 14
     });
 
-    ko.applyBindings(new viewModel(googleMap));
-
-    
+    ko.applyBindings(new viewModel(googleMap)); 
 }
+
+
 
 /**
 * @description Generate random number for Yelp request.
@@ -199,6 +228,9 @@ var YELP_KEY = "6CE4_UfXfQm9XiVp7ftIFg",
     YELP_TOKEN_SECRET = "Xw9V5sD5em4ulqeOvCTBUsLxoqI";
   
 
+var targetLat = target.latLng().lat;
+var targetLng = target.latLng().lng;
+
 var yelpURL = YELP_BASE_URL;
 
 var parameters = {
@@ -209,6 +241,7 @@ var parameters = {
   oauth_signature_method: 'HMAC-SHA1',
   oauth_version : '1.0',
   term: target.name(),
+  ll:targetLat + ", " + targetLng,
   limit:1,
   callback: 'cb'                  
   };
@@ -224,15 +257,15 @@ var settings = {
     cache: true,            
     dataType: 'jsonp',
     success: function(results) {
-        console.log(results);
+        console.log('success!');
+        target.marker.yelpInfo = results;
     },
-    fail: function() {
+    fail: function(results) {
+        console.log('failed');
+        console.log(results);
     }
     
   };
     
-    console.log(settings);
     $.ajax(settings);
-    
-  
 }
